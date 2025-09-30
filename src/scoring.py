29dee6555aee910fda607cfad247f10881b2b4c0
kpudleiner <pudleiner.kailyn@gmail.com>
2025-09-29 22:45:14 -0400

    create scoring class... that works!!!

--- src/scoring.py
+++ src/scoring.py
@@ -35,39 +35,47 @@ import numpy as np
 #db.run_action(sql, commit=True)
  
 
-deck = np.load(f'../Decks/DeckStack_0_10000.npy')[0]
-print(deck)
+# deck = np.load(f'../Decks/DeckStack_0_10000.npy')[0]
+# print(deck)
 
-deck_str = ''.join(map(str, deck))
-print(deck_str)
-
-def find_first_pattern(deck, patterns):
-    indices = {pattern: deck.find(pattern) for pattern in patterns}
-    if -1 in indices.values():
-        #end_game(indices)
-        print('game over')
-    return indices
+# deck_str = ''.join(map(str, deck))
+# print(deck_str)
 
 # def find_first_pattern(deck, patterns):
-#     indices = {}
-#     for pattern in patterns:
-#         for i in range(len(deck) - 2):
-#             if np.array_equal(deck[i:i+3], pattern):
-#                 indices[''.join(map(str, pattern))] = i+3
-#                 break
-#         else:
-#             indices[''.join(map(str, pattern))] = None  # Not found
+#     indices = {pattern: deck.find(pattern) + 3 for pattern in patterns}
+#     if 2 in indices.values():
+#         #end_game(indices)
+#         print('one pattern not found')
+#         indices = {k: (None if v == 2 else v) for k, v in indices.items()}
+#     else:
+#         indices = {pattern: deck.find(pattern) for pattern in patterns}
 #     return indices
 
-patterns = ['111', '001']
-
-indicies = find_first_pattern(deck_str, patterns)
-print(indicies)
+# patterns = ['111', '001']
 
 # p1_tricks = 0
 # p2_tricks = 0
 # p1_cards = 0
 # p2_cards = 0
+
+# cards_left = len(deck_str)
+# while len(deck_str) > 2:
+#     indices = find_first_pattern(deck_str, patterns)
+#     print(indices)
+#     indices_vals = list(indices.values())
+#     if indices_vals[0] < indices_vals[1]:
+#         p1_tricks += 1
+#         p1_cards += indices_vals[0]
+#         cards_left = cards_left - p1_cards
+#     else:
+#         p2_tricks +=1
+#         p2_cards += indices_vals[1]
+#         cards_left = cards_left - p2_cards
+#     print(p1_tricks, p2_tricks)
+#     print(p1_cards, p2_cards)
+#     deck_str = deck_str[min(indices.values()):]
+#     print(deck_str)
+
 
 # cards_left = 52
 # while cards_left > 2:
@@ -75,3 +83,74 @@ print(indicies)
 
 
 
+
+class ScoringDeck:
+    """
+    """
+
+    def __init__(self, cards: str , p1: str, p2:str ):
+
+        DECK_SIZE = 52
+
+        self.cards = cards
+        self.cards_left = DECK_SIZE
+
+        self.p1 = p1
+        self.p2 = p2
+
+        self.p1_tricks = 0
+        self.p2_tricks = 0
+        self.p1_cards = 0
+        self.p2_cards = 0
+
+
+    def __repr__(self):
+        return f"DeckStack(cards={self.cards})"
+
+    def score_deck(self):
+        patterns = [self.p1, self.p2]
+        print(patterns)
+        while self.cards_left > 3:
+            indices = {pattern: self.cards.find(pattern) + 3 for pattern in patterns}
+            #indices = find_first_pattern(deck_str, patterns)
+            print(indices)
+            indices_vals = list(indices.values())
+            if indices_vals[0] == 2 and indices_vals[1] == 2:
+                print('game over')
+                break
+            elif indices_vals[0] < indices_vals[1] and indices_vals[0] != 2:
+                self.p1_tricks += 1
+                self.p1_cards += indices_vals[0]
+                self.cards_left = self.cards_left - indices_vals[0]
+            else:
+                self.p2_tricks +=1
+                self.p2_cards += indices_vals[1]
+                self.cards_left = self.cards_left - indices_vals[1]
+
+            print(self.p1_tricks, self.p2_tricks)
+            print(self.p1_cards, self.p2_cards)
+
+            if indices_vals[0] == 2: cards_gone = indices_vals[1]
+            elif indices_vals[1] == 2: cards_gone = indices_vals[0]
+            else: cards_gone = min(indices.values())
+            self.cards = self.cards[cards_gone:]
+            print(self.cards)
+            print(self.cards_left)
+
+        final_counts = {'p1_tricks': self.p1_tricks, 
+                        'p2_tricks': self.p2_tricks,
+                        'p1_cards': self.p1_cards,
+                        'p2_cards': self.p2_cards
+                        }
+        return final_counts
+    
+deck = np.load(f'../Decks/DeckStack_0_10000.npy')[0]
+print(deck)
+
+deck_str = ''.join(map(str, deck))
+print(deck_str)
+
+test = ScoringDeck(deck_str, '111', '001')
+print(test.cards)
+print(test.cards_left)
+final_counts = test.score_deck()
