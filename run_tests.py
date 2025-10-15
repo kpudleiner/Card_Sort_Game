3272f0527bcd229c3d5597ba9451de94e48abc65
kpudleiner <pudleiner.kailyn@gmail.com>
2025-10-14 20:37:41 -0400

    710,000 decks

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ from src.scoring.scoring_methods import score_all_unscored_decks, save_player_sc
 from src.scoring.base_db import BaseDB
 from src.scoring.deck_scoring_db_create import reset_db
 import pandas as pd
+import os
 
 # decks_npy = DeckStackNpy(10000)
 # decks_npy.save_decks()
@@ -56,3 +57,42 @@ reset_db()
 # reset_db()
 
 # print(pd.read_csv('src/scoring/player_wins.csv'))
+
+###################################################################
+
+# def augment_decks(num_decks):
+#     if num_decks < 10000:
+#         decks_npy = DeckStackNpy(num_decks)
+#         decks_npy.save_decks()
+#     else:
+#         num_10000 = num_decks//10000
+#         print(num_10000)
+#         remaining_decks = num_decks % 10000
+#         print(remaining_decks)
+#         for i in range(num_10000):
+#             decks_npy = DeckStackNpy(10000)
+#             decks_npy.save_decks()
+#         if remaining_decks != 0:
+#             decks_npy = DeckStackNpy(remaining_decks)
+#             decks_npy.save_decks()
+
+#     print(f'Scoring {num_decks} new decks.')
+
+#     db = BaseDB(path='src/scoring/deck_scoring.sqlite')
+#     sql = """
+#         SELECT COUNT(*) from deck_scores;
+#         """
+#     print(db.run_query(sql))
+
+#     score_all_unscored_decks()
+
+#     print(db.run_query(sql))
+
+#     save_player_scores()
+
+#     print('Updating Figures')
+
+# for i in range(200):
+#     os.remove('src/scoring/deck_scoring.sqlite')
+#     reset_db()
+#     augment_decks(50000)
